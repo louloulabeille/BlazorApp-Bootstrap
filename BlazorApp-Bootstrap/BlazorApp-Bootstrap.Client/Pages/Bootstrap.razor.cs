2f0c0d81201d4723434e352a16e2aec53d92a8e4
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-05-11 15:19:55 +0200

    Ajoutez des fichiers projet.

---
+++ BlazorApp-Bootstrap/BlazorApp-Bootstrap.Client/Pages/Bootstrap.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp_Bootstrap.Client.Pages
+{
+    public class BootstrapBase : ComponentBase
+    {
+        #region public properties Parameter
+        [Parameter]
+        public int Section { get; set; }
+        [Parameter]
+        public string? Partie { get; set; }
+        #endregion
+
+    }
+}
