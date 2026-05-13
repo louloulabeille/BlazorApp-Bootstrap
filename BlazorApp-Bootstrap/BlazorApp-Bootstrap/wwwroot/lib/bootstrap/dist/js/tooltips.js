a0ca3bfe7c86f81a760ecfcc1aff8bde5ca69a2f
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-05-13 13:19:32 +0200

    Fenêtre modal

--- BlazorApp-Bootstrap/BlazorApp-Bootstrap/wwwroot/lib/bootstrap/dist/js/tooltips.js
+++ BlazorApp-Bootstrap/BlazorApp-Bootstrap/wwwroot/lib/bootstrap/dist/js/tooltips.js
@@ -1,2 +1,12 @@
 ﻿const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+// - donner le focus sur le bouton btn-Modal-Close
+
+const myModal = document.getElementById('exampleModal')
+const myInput = document.getElementById('btn-Modal-Close')
+
+myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+})
