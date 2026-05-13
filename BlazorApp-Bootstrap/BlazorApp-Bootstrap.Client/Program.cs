1103857a7408f887c47e06f3edaf4ee154e6c798
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-05-13 10:33:24 +0200

    Progress bar

--- BlazorApp-Bootstrap/BlazorApp-Bootstrap.Client/Program.cs
+++ BlazorApp-Bootstrap/BlazorApp-Bootstrap.Client/Program.cs
@@ -1,5 +1,10 @@
+using BlazorApp_Bootstrap.Infrastructure;
+using BlazorApp_Bootstrap.Interface;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// - injection de dépendance au niveau de l'assembly il faut le rajouter aussi sinon erreur javascript
+builder.Services.AddTransient<IManageForm, MemoryManageForm>();
 
 await builder.Build().RunAsync();
