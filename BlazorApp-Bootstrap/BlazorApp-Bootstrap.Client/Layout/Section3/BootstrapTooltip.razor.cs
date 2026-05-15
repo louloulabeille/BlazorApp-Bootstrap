e15320e88b519635f7228b9320c1c416cd22e23e
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-05-15 15:21:49 +0200

    Mise en place de tooltip boostrap rajouter du jacript pour les créer manuellement sur la page

---
+++ BlazorApp-Bootstrap/BlazorApp-Bootstrap.Client/Layout/Section3/BootstrapTooltip.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
+
+namespace BlazorApp_Bootstrap.Client.Layout.Section3
+{
+    /// <summary>
+    /// Pour le boostrap Tooltip - il faut  mettre en place du jascript pour le faire marcher
+    /// c'est à dire qu'il faut les créer en javascript manuellement au niveau de la page
+    /// après la fin du rendu de la page
+    /// </summary>
+    public class BootstrapTooltipBase : ComponentBase 
+    {
+        #region public inject properties
+        [Inject] IJSRuntime JSRuntime { get; set; } = default!;
+
+        #endregion
+
+        #region protected override method
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSRuntime.InvokeVoidAsync("initTooltips");
+            }
+            await base.OnAfterRenderAsync(firstRender);
+        }
+        #endregion
+    }
+}
---
+++ BlazorApp-Bootstrap/BlazorApp-Bootstrap.Client/wwwroot/script/tooltips.js
@@ -0,0 +1,7 @@
+﻿window.initTooltips = () => {
+    var tlist = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tlist.forEach(function (el) {
+        bootstrap.Tooltip.getOrCreateInstance(el);
+
+    })
+}
