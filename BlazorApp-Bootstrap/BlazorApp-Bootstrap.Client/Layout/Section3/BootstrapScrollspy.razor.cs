1e0d5ba94ff0950e72920327a719f75d3dd5e074
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-05-15 14:30:33 +0200

    mise en place d'un scrollspy de bootstrap - qui normalement ne marche pas sur Blazor mais il faut 2 fonctions javascripts scrollSpy - un event de nav et l'appel de fonction javascript dans la page

---
+++ BlazorApp-Bootstrap/BlazorApp-Bootstrap.Client/Layout/Section3/BootstrapScrollspy.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
+
+namespace BlazorApp_Bootstrap.Client.Layout.Section3
+{
+    public class BootstrapScrollspyBase : ComponentBase, IDisposable
+    {
+        #region properties inject 
+        [Inject] public IJSRuntime JSRuntime { get; set; } = default!;
+        [Inject] public NavigationManager Nav { get; set; } = default!;
+        #endregion
+
+        #region protected override method
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstRender"></param>
+        /// <returns></returns>
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            try
+            {
+                await JSRuntime.InvokeVoidAsync("initScrollSpy");
+                Nav.LocationChanged += OnLocationChanged;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            
+        }
+        #endregion
+
+        #region private method
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnLocationChanged(object? sender, LocationChangedEventArgs e)
+        {
+            _ = JSRuntime.InvokeVoidAsync("refreshScrollSpy");
+        }
+        #endregion
+
+        #region public method
+        public void Dispose()
+        {
+            Nav.LocationChanged -= OnLocationChanged;
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+}
---
+++ BlazorApp-Bootstrap/BlazorApp-Bootstrap.Client/wwwroot/script/scrollSpy.js
@@ -0,0 +1,11 @@
+﻿window.initScrollSpy = () => {
+    var el = document.querySelector('[data-bs-spy="scroll"]');
+    if (!el) return;
+    bootstrap.ScrollSpy.getOrCreateInstance(el);
+}
+
+window.refreshScrollSpy = () => {
+    var el = document.querySelector('[data-bs-spy="scroll"]');
+    var inst = bootstrap.ScrollSpy.getInstance(el);
+    if (inst) inst.refresh();
+}
